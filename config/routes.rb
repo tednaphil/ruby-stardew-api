80906d716d54df767b3064714e0ef027f6f9879f
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-07-31 11:43:45 -0500

    Add user controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
   get "/characters", to: "characters#index"
+
+  resources :users
+
+  resources  :user_friends
+
 
   # Defines the root path route ("/")
   # root "posts#index"
