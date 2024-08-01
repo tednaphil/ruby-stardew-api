066cec220e5a0754038d3a4c02ca704d3a8a6a65
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-07-31 21:37:41 -0500

    Add Character model methods and refactor rendered json

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,10 +1,22 @@
 class CharactersController < ApplicationController
   def show
-    render json: CharacterPresenter.new(Character.find(params[:id]))
-    
+    # render json: CharacterPresenter.new(Character.find(params[:id]))
+    @hobbies = Character.hobby_names(params[:id])
+    @fav_gifts = Character.fav_gift_names(params[:id])
+    @character = Character.data(params[:id])
+    render json: {
+      id: @character.id,
+      name: @character.name,
+      birthday: @character.birthday,
+      hobbies: @hobbies,
+      fav_gifts: @fav_gifts
+    }
   end
 
   def index
-    render json: CharactersPresenter.new(Character.all)
+    # render json: CharactersPresenter.new(Character.all)
+    @characters = Character.all
+    render json: @characters
+
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -7,4 +7,37 @@ class Character < ApplicationRecord
 
     has_many :character_hobbies
     has_many :hobbies, through: :character_hobbies
+
+    def self.hobby_names(char_id)
+        #write Active Record query
+            #find the character hobbies
+            #select name from the hobbies
+        @char_hobbies = Character.find(char_id).character_hobbies
+        @char_hobbies.map{ |h| Hobby.find([h.id]) }.flatten
+        # @names = @char_hobbies.each do |h| 
+        #     text = h :name
+        #     puts "Hi Tayla"
+        #     puts text
+        # end
+
+        # @names
+        # sql = "SELECT name FROM hobbies
+        # WHERE character_id = char_id"
+
+        # records_array = ActiveRecord::Base.connection.execute(sql)
+        
+        # put @names
+    end
+
+    def self.fav_gift_names(char_id)
+        @char_gifts = Character.find(char_id).character_gifts
+        @char_gifts.map{ |g| Gift.find([g.id]) }.flatten
+    end
+
+    def self.data(char_id)
+        Character.find(char_id)
+    end
+    
+
+
 end
