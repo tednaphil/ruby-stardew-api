6997f53d6ea1164233d6a4aa2dffa8b9db917925
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-08-02 17:40:24 -0500

    Seed gift and hobby data

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,5 @@
 class CharactersController < ApplicationController
   def show
-    # render json: CharacterPresenter.new(Character.find(params[:id]))
     @hobbies = Character.hobby_names(params[:id])
     @fav_gifts = Character.fav_gift_names(params[:id])
     @character = Character.data(params[:id])
@@ -14,7 +13,6 @@ class CharactersController < ApplicationController
   end
 
   def index
-    # render json: CharactersPresenter.new(Character.all)
     # @characters = Character.all
     # @characters.each do |c|
     #   puts c
